d7e269b49abe4587f1dda1d4ae1b488206f99aa1
wpdmitry <wpdmitry@yandex.ru>
2019-10-16 20:03:30 +0300

    add i-weather page

---
+++ i-weather/precache-manifest.0628ece2090afcfd2ecab676bab7f083.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fc8db0ead2d484485483e2c8b6cfacd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "18448c24ca4d568ab060",
+    "url": "/static/css/main.7f8bb215.chunk.css"
+  },
+  {
+    "revision": "684181041ea4410921d2",
+    "url": "/static/js/2.5bf7cc41.chunk.js"
+  },
+  {
+    "revision": "18448c24ca4d568ab060",
+    "url": "/static/js/main.2bc9c9d7.chunk.js"
+  },
+  {
+    "revision": "736ebf37220533128068",
+    "url": "/static/js/runtime-main.61c623a1.js"
+  }
+]);

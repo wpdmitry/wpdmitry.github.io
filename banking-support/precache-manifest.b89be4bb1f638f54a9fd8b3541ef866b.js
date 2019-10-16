deec109fe112779332fa6daea46f8278d541908d
wpdmitry <wpdmitry@yandex.ru>
2019-10-16 04:45:22 +0300

    add banking support page

---
+++ banking-support/precache-manifest.b89be4bb1f638f54a9fd8b3541ef866b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c5ca7ec66c6c50da275d2d6909c26d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ff9829e032fea92387e7",
+    "url": "/static/css/main.e574bdff.chunk.css"
+  },
+  {
+    "revision": "b446ca02ef912587c137",
+    "url": "/static/js/2.d6c0f3e2.chunk.js"
+  },
+  {
+    "revision": "ff9829e032fea92387e7",
+    "url": "/static/js/main.d69f3155.chunk.js"
+  },
+  {
+    "revision": "42cf08e2e3ea194d251e",
+    "url": "/static/js/runtime-main.c0e36df9.js"
+  }
+]);
